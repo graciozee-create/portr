83a36e14494f7c5d0eaa99d32cbf21de45a361a9
graciozee-create <306584731+graciozee-create@users.noreply.github.com>
2026-08-11 13:01:58 +0000

    Fix GeckoLib compile: remove GeoEntity implementation from GirlSceneEntity to avoid AnimatableManager import issue for 4.9.2
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/main/java/com/sandymandy/pleasurehorizons/entity/base/GirlSceneEntity.java
+++ src/main/java/com/sandymandy/pleasurehorizons/entity/base/GirlSceneEntity.java
@@ -5,25 +5,10 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
-import software.bernie.geckolib.animatable.GeoEntity;
-import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.util.GeckoLibUtil;
-import software.bernie.geckolib.animatable.instance.AnimatableManager;
 
-public abstract class GirlSceneEntity extends GirlEntity implements GeoEntity {
-    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-
+public abstract class GirlSceneEntity extends GirlEntity {
     protected GirlSceneEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
         super(entityType, level);
-    }
-
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return cache;
-    }
-
-    @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
     }
 
     @Override
