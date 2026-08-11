667be5aa779a0a195ded0708d625afd1dff767c5
graciozee-create <306584731+graciozee-create@users.noreply.github.com>
2026-08-11 13:06:42 +0000

    Fix GeckoLib 4.9.2 import: use software.bernie.geckolib.animatable.manager.AnimatableManager (was core.animatable.instance)
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/main/java/com/sandymandy/pleasurehorizons/entity/base/GirlSceneEntity.java
+++ src/main/java/com/sandymandy/pleasurehorizons/entity/base/GirlSceneEntity.java
@@ -5,10 +5,25 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.util.GeckoLibUtil;
+import software.bernie.geckolib.animatable.manager.AnimatableManager;
 
-public abstract class GirlSceneEntity extends GirlEntity {
+public abstract class GirlSceneEntity extends GirlEntity implements GeoEntity {
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+
     protected GirlSceneEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
         super(entityType, level);
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
     }
 
     @Override
