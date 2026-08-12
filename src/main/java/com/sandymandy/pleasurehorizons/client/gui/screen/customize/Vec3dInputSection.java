883d49206be259c92c5bafbb82f5747e9d58cdb4
graciozee-create <306584731+graciozee-create@users.noreply.github.com>
2026-08-12 10:03:03 +0000

    Test Vec3dInputSection minimal with Vec3 in signature
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/main/java/com/sandymandy/pleasurehorizons/client/gui/screen/customize/Vec3dInputSection.java
+++ src/main/java/com/sandymandy/pleasurehorizons/client/gui/screen/customize/Vec3dInputSection.java
@@ -1,5 +1,31 @@
 package com.sandymandy.pleasurehorizons.client.gui.screen.customize;
 
-public class Vec3dInputSection {
-    public Vec3dInputSection() {}
+import com.sandymandy.pleasurehorizons.client.gui.screen.CustomizeScreen;
+import com.sandymandy.pleasurehorizons.entity.base.GirlSceneEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.network.chat.Component;
+
+public class Vec3dInputSection<T extends GirlSceneEntity> extends CustomizeSection<T> {
+
+    private final String label;
+
+    public Vec3dInputSection(T entity, T previewEntity, String label,
+                             java.util.function.Supplier<net.minecraft.world.phys.Vec3> valueGetter,
+                             java.util.function.Consumer<net.minecraft.world.phys.Vec3> valueSetter) {
+        super(entity, previewEntity);
+        this.label = label;
+    }
+
+    @Override
+    public void init(CustomizeScreen<T> screen, CustomizeScreen.LayoutConfig layout, int startY) {
+    }
+
+    @Override
+    public int render(CustomizeScreen<T> screen, CustomizeScreen.LayoutConfig layout, int currentY) {
+        StringWidget labelWidget = new StringWidget(layout.centerX, currentY, layout.contentWidth, 20,
+                Component.literal(label), Minecraft.getInstance().font);
+        screen.addWidget(labelWidget);
+        return currentY + 20;
+    }
 }
