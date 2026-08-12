87350d271de631cc6c9c0e454ce10e67379e14e5
graciozee-create <306584731+graciozee-create@users.noreply.github.com>
2026-08-12 11:56:52 +0000

    SliderSection with Component overloads
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/main/java/com/sandymandy/pleasurehorizons/client/gui/screen/customize/SliderSection.java
+++ src/main/java/com/sandymandy/pleasurehorizons/client/gui/screen/customize/SliderSection.java
@@ -12,17 +12,35 @@ import java.util.function.Supplier;
 
 public class SliderSection<T extends GirlSceneEntity> extends CustomizeSection<T> {
 
-    private final String label;
+    private final Component label;
+    private final int minValue;
+    private final int maxValue;
+    private final Supplier<Integer> valueGetter;
+    private final Consumer<Integer> valueSetter;
 
-    public SliderSection(T entity, T previewEntity, String label, int minValue, int maxValue,
-                         Supplier<Integer> valueGetter, Consumer<Integer> valueSetter, String tooltipText) {
+    public SliderSection(T entity, T previewEntity, Component label, int minValue, int maxValue,
+                         Supplier<Integer> valueGetter, Consumer<Integer> valueSetter) {
         super(entity, previewEntity);
         this.label = label;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.valueGetter = valueGetter;
+        this.valueSetter = valueSetter;
     }
 
     public SliderSection(T entity, T previewEntity, String label, int minValue, int maxValue,
                          Supplier<Integer> valueGetter, Consumer<Integer> valueSetter) {
-        this(entity, previewEntity, label, minValue, maxValue, valueGetter, valueSetter, null);
+        this(entity, previewEntity, Component.literal(label), minValue, maxValue, valueGetter, valueSetter);
+    }
+
+    public SliderSection(T entity, T previewEntity, Component label, int minValue, int maxValue,
+                         Supplier<Integer> valueGetter, Consumer<Integer> valueSetter, String tooltip) {
+        this(entity, previewEntity, label, minValue, maxValue, valueGetter, valueSetter);
+    }
+
+    public SliderSection(T entity, T previewEntity, String label, int minValue, int maxValue,
+                         Supplier<Integer> valueGetter, Consumer<Integer> valueSetter, String tooltip) {
+        this(entity, previewEntity, Component.literal(label), minValue, maxValue, valueGetter, valueSetter);
     }
 
     @Override
@@ -31,13 +49,27 @@ public class SliderSection<T extends GirlSceneEntity> extends CustomizeSection<T
 
     @Override
     public int render(CustomizeScreen<T> screen, CustomizeScreen.LayoutConfig layout, int currentY) {
+        int value = valueGetter.get();
+
         StringWidget labelWidget = new StringWidget(layout.centerX, currentY, layout.contentWidth, 20,
-                Component.literal(label), Minecraft.getInstance().font);
+                Component.literal(label.getString() + \": \" + value), Minecraft.getInstance().font);
         screen.addWidget(labelWidget);
         currentY += 20;
 
-        Button minusBtn = Button.builder(Component.literal("-"), btn -> {}).bounds(layout.centerX, currentY, 50, 20).build();
-        Button plusBtn = Button.builder(Component.literal("+"), btn -> {}).bounds(layout.centerX + 54, currentY, 50, 20).build();
+        int btnWidth = (layout.contentWidth / 2) - 2;
+        Button minusBtn = Button.builder(Component.literal(\"-\"), btn -> {
+            int cur = valueGetter.get();
+            int newVal = cur - 1;
+            if (newVal < minValue) newVal = minValue;
+            valueSetter.accept(newVal);
+        }).bounds(layout.centerX, currentY, btnWidth, 20).build();
+
+        Button plusBtn = Button.builder(Component.literal(\"+\"), btn -> {
+            int cur = valueGetter.get();
+            int newVal = cur + 1;
+            if (newVal > maxValue) newVal = maxValue;
+            valueSetter.accept(newVal);
+        }).bounds(layout.centerX + btnWidth + 4, currentY, btnWidth, 20).build();
 
         screen.addWidget(minusBtn);
         screen.addWidget(plusBtn);
